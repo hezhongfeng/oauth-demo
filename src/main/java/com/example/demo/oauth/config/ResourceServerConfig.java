489e3713abe359e26133fd6aede821bf4e5afaae
hezf <287645587@qq.com>
2023-06-14 14:37:40 +0800

    删除一些无效import

--- src/main/java/com/example/demo/oauth/config/ResourceServerConfig.java
+++ src/main/java/com/example/demo/oauth/config/ResourceServerConfig.java
@@ -6,12 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
@@ -30,11 +25,4 @@ public class ResourceServerConfig {
 				.jwt();
 		return http.build();
 	}
-
-	// // 这个很关键，给ResourceServer解码用，和认证的过程一样的解码
-  // @Bean
-  // JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-  //   return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-  // }
-	// @formatter:on
 }
